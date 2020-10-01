0ca8d308c91f8a1493a246fb833b32b1da4be4c3
varun-kolli <vkolli@calpoly.edu>
2020-09-30 20:50:43 -0700

    more cases

--- stack_tests.py
+++ stack_tests.py
@@ -3,7 +3,7 @@ import unittest
 # Use the imports below to test either your array-based stack
 # or your link-based version
 from stack_array import Stack
-from stack_linked import Stack
+#from stack_linked import Stack
 
 class TestLab2(unittest.TestCase):
     def test_simple(self):
@@ -15,6 +15,23 @@ class TestLab2(unittest.TestCase):
         stack.push(1)
         self.assertEqual(stack.pop(), 1)
         self.assertEqual(stack.peek(), 0)
+
+    def test_empty(self):
+        stack = Stack(5)
+        self.assertTrue(stack.is_empty())
+        stack.push(1)
+        self.assertFalse(stack.is_empty())
+
+    def test_full(self):
+        stack = Stack(1)
+        self.assertFalse(stack.is_full())
+        stack.push(1)
+        self.assertTrue(stack.is_full())
+
+    def test_size(self):
+        stack = Stack(1)
+        stack.push(1)
+        self.assertEqual(stack.size(), 1)
 
     def test_push_error(self):
         stack = Stack(1)
@@ -29,8 +46,11 @@ class TestLab2(unittest.TestCase):
 
     def test_peek_error(self):
         stack = Stack(1)
+        x = stack
         with self.assertRaises(IndexError):
             stack.peek()
+        self.assertEqual(x, stack)
+
 
 
 
